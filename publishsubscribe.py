d738e80b02ef55b19201908c3846c1ec3968d4a1
Kevin Chung <kwc15@sfu.ca>
2015-04-18 22:16:29 -0700

    Made changes to make publish/subscribe work along with the algorithm

--- publishsubscribe.py
+++ publishsubscribe.py
@@ -4,8 +4,6 @@
     and operation and publish it to other instances.
 '''
 
-#imports from frontend.py, not sure if I need them
-from frontend import get_ports
 import zmq
 '''
     Takes gathers the sequence number and operation of an instance
@@ -14,52 +12,27 @@ import zmq
     the list of sockets (5 seconds per entry) for new events.
 '''
 
-def send_message(pub_port, message):    
-    print "Instance: " + instance + " publishing: " + message
-    pub_port.send_string(message)
-    return {
-        "data": {
-            "type": "Message sent"
-            }
-        }
-def receive_message(sub_ports):
+def send_message(pub_socket, message):    
+	print "Instance: " + instance + " publishing: " + message
+	pub_socket.send_string(message)
 
-    if len(sub_ports) == 0:
-        response.status = 404
-        return {
-            "errors": [{
-                "no subscriptions": {
-                    "msg": "This instance is not subscribed to any publishers"
-                    }
-                }]
-            }
+def receive_message(sub_sockets):
+	if len(sub_sockets) == 0:
+		response.status = 404
+		return "No Subscriptions", False
 
-    else:
-        for (index in len(sub_ports)):
-            num_events = sub_ports[index].poll(timeout=5)
-            if (num_events > 0):
-                try:
-                    message_content = sub_ports[index].recv(zmq.NOBLOCK)
-                    return {
-                        "data": {
-                            "type": "message",
-                            "msg": message_content
-                        }
-                    }
-                except zmq.ZMQError as zerr:
-                    if zerr.errno == zmq.EAGAIN:
-                        #no messages found
-                        return {
-                            "data": {
-                                "type": "empty_message",
-                            }
-                        }
-                    else:
-                        # Not an empty message, but worse
-                        raise zerr
+	else:
+		for (index in len(sub_sockets)):
+			num_events = sub_sockets[index].poll(timeout=5)
+			if (num_events > 0):
+				try:
+					message_content = sub_sockets[index].recv(zmq.NOBLOCK)
+					# TODO: Format of message. Thinking that it should be:
+						# [Seq_num, Message]
+					return message_content, True
 
-    
-listOfPorts = gen_ports() #get ports, first is publish, rest is list of subscriber
-send_message(listOfPorts[0], message)
-receive_message(listOfPorts[1:])
-    
+				except zmq.ZMQError as zerr:
+					if zerr.errno == zmq.EAGAIN:
+						return "Empty Message", False
+						# Not an empty message, but worse
+						raise zerr
